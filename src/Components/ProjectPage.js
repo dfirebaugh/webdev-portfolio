3f909dca7498bb4af23284aeb26f440d5f2a27e1
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-03-12 22:51:44 -0400

    start on project page imrpovements

--- src/Components/ProjectPage.js
+++ src/Components/ProjectPage.js
@@ -7,10 +7,21 @@ const findProject = name => {
 
   return finder(projects) || finder(proProjects);;
 }
+
 const Project = props => {
   const project = findProject(props.match.params.projectName)
   console.log(project)
-
+  const cardContainerStyle = {
+    width: '100%',
+    height: '50vh',
+    color: 'white',
+    // backgroundImage: `url(${image})`,
+    boxShadow: 'inset 0 0 0 100px rgba(36, 70, 105, 0.6)'
+  }
+  const cardStyle = {
+    color: 'white',
+    width: '100%'
+  }
   if (!project) {
     return <h1> There is no project with that name! </h1>
   }
@@ -18,31 +29,24 @@ const Project = props => {
 
   return (
     <div>
-      <h3>
-        {project.name}
-      </h3>
-      <div className="cardContainer">
-        <a className="cardTitle" href={url}>
-          <div className="card text-black bg-light" >
-            <div className="card-body" >
-              <h4>{title}</h4>
-              <div>
-
-                <br></br>
-              </div>
-              <footer className="cardFooter">Technologies:
-                <div>
-                  {tech}
-                </div>
-              </footer>
+      <a style={cardStyle} href={url}>
+        <h3>
+          {project.name}
+        </h3>
+      </a>
+      <div>
+        <div className=" text-black bg-light" style={cardContainerStyle}>
+          <footer className="cardFooter">Technologies:
+            <div>
+              {tech}
             </div>
-          </div>
-        </a>
+          </footer>
+        </div>
         {github ? <a href={github}>github</a> : ""}
         {codepen ? <a href={codepen}>codepen</a> : ""}
 
       </div>
-    </div>
+    </div >
   )
 }
 
