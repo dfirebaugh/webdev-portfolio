f100bcaa8373b3a8221888c7b9019fbd3305c8d9
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-03-12 22:46:31 -0400

    addig the gopher

--- src/Components/Logos.js
+++ src/Components/Logos.js
@@ -4,10 +4,12 @@ import reactLogo from '../images/react.svg';
 import html5 from '../images/HTML5_logo.svg';
 import javascript from '../images/javascript.svg';
 import css3 from '../images/css3.svg';
+import gopher from '../images/gopher.png';
 
 const Logos = () => {
   return (
     <div className="sideMenu container">
+      <img src={gopher} className="App-logo" alt="gopher" />
       <img src={html5} className="App-logo" alt="logo" />
       <img src={css3} className="App-logo" alt="logo" />
       <img src={javascript} className="App-logo" alt="logo" />
