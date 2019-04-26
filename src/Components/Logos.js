939b1a7d6f8d28e96c63c2d7243b1b2699a97b94
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-04-26 00:34:11 -0400

    using more functional components

--- src/Components/Logos.js
+++ src/Components/Logos.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
-import jquery from '../images/JQuery_logo.svg';
-import reactLogo from '../images/react.svg';
-import html5 from '../images/HTML5_logo.svg';
-import javascript from '../images/javascript.svg';
-import css3 from '../images/css3.svg';
+import React from "react";
+import jquery from "../images/JQuery_logo.svg";
+import reactLogo from "../images/react.svg";
+import html5 from "../images/HTML5_logo.svg";
+import javascript from "../images/javascript.svg";
+import css3 from "../images/css3.svg";
 
-class Logos extends Component {
-  render() {
-    return (
-      <div className="sideMenu container">
+const Logos = () => {
+  return (
+    <div className="sideMenu container">
       <img src={html5} className="App-logo" alt="logo" />
       <img src={css3} className="App-logo" alt="logo" />
       <img src={javascript} className="App-logo" alt="logo" />
       <img src={reactLogo} className="App-logo" alt="logo" />
       <img src={jquery} className="App-logo" alt="logo" />
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Logos;
--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,45 +1,40 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class ProjectCard extends Component {
-  render() {
-    const cardContainerStyle = {
-      // border: '5px solid pink',
-      width: '16em',
-      height: '12em',
-      color: 'white',
-      backgroundImage: `url(${this.props.image})`,
-      boxShadow: 'inset 0 0 0 100px rgba(36, 70, 105, 0.6)'
-    }
-    const cardStyle = {
-      color: 'white'
-    }
-    let techBtns = this.props.techs.map(function (item, index) {
-      return item + " ";
-    })
-    return (
-      <div className="cardContainer">
-        <a className="cardTitle" style={cardStyle} href={this.props.url}>
-          <div className="card text-black bg-light" style={cardContainerStyle}>
-            <div className="card-body" >
-              <h4>{this.props.title}</h4>
-              <div>
-
-                <br></br>
-              </div>
-              <footer className="cardFooter">Technologies:
-                <div>
-                  {techBtns}
-                </div>
-              </footer>
+const ProjectCard = props => {
+  const cardContainerStyle = {
+    // border: '5px solid pink',
+    width: "16em",
+    height: "12em",
+    color: "white",
+    backgroundImage: `url(${props.image})`,
+    boxShadow: "inset 0 0 0 100px rgba(36, 70, 105, 0.6)"
+  };
+  const cardStyle = {
+    color: "white"
+  };
+  let techBtns = props.techs.map(function(item, index) {
+    return item + " ";
+  });
+  return (
+    <div className="cardContainer">
+      <a className="cardTitle" style={cardStyle} href={props.url}>
+        <div className="card text-black bg-light" style={cardContainerStyle}>
+          <div className="card-body">
+            <h4>{props.title}</h4>
+            <div>
+              <br />
             </div>
+            <footer className="cardFooter">
+              Technologies:
+              <div>{techBtns}</div>
+            </footer>
           </div>
-        </a>
-        {this.props.github ? <a href={this.props.github}>github</a> : ""}
-        {this.props.codepen ? <a href={this.props.codepen}>codepen</a> : ""}
-
-      </div>
-    );
-  }
-}
+        </div>
+      </a>
+      {props.github ? <a href={props.github}>github</a> : ""}
+      {props.codepen ? <a href={props.codepen}>codepen</a> : ""}
+    </div>
+  );
+};
 
 export default ProjectCard;
